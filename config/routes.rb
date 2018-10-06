a0d597113d15461ab5136f1e0ec073079447c4cb
YoofiBP <joseph.brown-pobee@ashesi.edu.gh>
2018-10-06 12:40:49 +0000

    Index removed

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   get 'labels/contact'
   get 'labels/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#index'
 end
