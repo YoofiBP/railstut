67ca94f7d63d71a5214f81a133f0d0d33a79e6dd
YoofiBP <joseph.brown-pobee@ashesi.edu.gh>
2018-10-06 12:56:00 +0000

    'Homepage'

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'labels/index'
+  root 'labels#index'
   get 'labels/contact'
   get 'labels/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
