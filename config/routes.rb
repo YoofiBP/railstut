7ee3db79622e14df7c46c9546a4b590423e2405c
YoofiBP <joseph.brown-pobee@ashesi.edu.gh>
2018-10-12 10:07:45 +0000

    Users Added!

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/homepage'
+  devise_for :users
+  devise_for :models
+  root to: 'pages#homepage'
   get 'pages/contact'
   get 'pages/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
