c348d799323c3bd104c8bea6bf1900d955967b2d
YoofiBP <joseph.brown-pobee@ashesi.edu.gh>
2018-10-18 12:39:37 +0000

    Added and styled articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :articles
   devise_for :users, :controllers => { registrations: 'registrations'}
+    resources :articles
   devise_for :models
   root to: 'pages#homepage'
   get 'pages/contact'
