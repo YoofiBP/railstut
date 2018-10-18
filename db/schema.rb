109ea0aeb48f389907f7661371623850852b329d
YoofiBP <joseph.brown-pobee@ashesi.edu.gh>
2018-10-18 18:00:48 +0000

    Associated articles with users

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_18_114029) do
+ActiveRecord::Schema.define(version: 2018_10_18_144748) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "models", force: :cascade do |t|
